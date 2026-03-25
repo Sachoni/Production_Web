42df41a6628a654784b51c7b966a744b6c90f446
Sachoni <wamukotajohnmark@gmail.com>
2026-03-25 12:31:19 +0300

    SQL Upload & 2 files

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 git add .
-git commit -m "🔥 Auto-update: %date% %time%"
+git commit -m "SQL Upload & 2 files"
 git pull origin main --rebase
 git push origin main
 echo ✅ Update complete! Check your website in a few minutes. 🚀
